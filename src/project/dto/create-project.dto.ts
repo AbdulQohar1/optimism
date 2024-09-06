509471fdbd4f695d1527c392933ec7d6c75a0536
AbdulQohar1 <adebayoabdulqohar1@gmail.com>
2024-09-06 11:10:34 +0100

    Staging create-project api

---
+++ src/project/dto/create-project.dto.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaClient, project } from "@prisma/client";
+
+@Injectable() 
+
+export class CreateProjectDto {
+	constructor (private readonly prisma: PrismaClient) {}
+
+	async createProject( data: {
+		id: number;
+    project_id: string;
+    name: string | null;
+    description: string | null;
+    created_by: string;
+    created_at: Date;
+    total_fund: number | null;
+    status: string | null;
+    project_logo_image: Buffer | null;
+    project_logo_name: string | null;
+    project_banner_image: Buffer | null;
+    website_url: string | null;
+	}): Promise< project> {
+		const project = await this.prisma.project.create({
+			data,
+		});
+		return project
+	};
+}
+
+
+// description TEXT,
+//   created_by VARCHAR(255) NOT NULL,
+//   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+//   total_fund INTEGER DEFAULT 0,
+//   status VARCHAR(50),
+//   project_logo_image BYTEA, 
+//   project_logo_name  VARCHAR(120),
+//   project_banner_image BYTEA,  
+//   website_url VARCHAR(255) 
+// // 
---
+++ src/project/project.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
+import { PrismaClient} from '@prisma/client';
+
+@Injectable()
+export class ProjectService {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async createProject (createProjectDto: projectCreateInput): Promise<  PublicProject> {
+    const project = await this.prisma.project.create({
+      data: createProjectDto,
+    });
+    return project;
+  }
+  // create(createProjectDto: CreateProjectDto) {
+  //   return 'This action adds a new project';
+  // }
+
+  // findAll() {
+  //   return `This action returns all project`;
+  // }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} project`;
+  // }
+
+  // update(id: number, updateProjectDto: UpdateProjectDto) {
+  //   return `This action updates a #${id} project`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} project`;
+  // }
+}
+
